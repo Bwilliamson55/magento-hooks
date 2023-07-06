672befdb9dd29c269c4c0db5314525ac21a487fe
bwilliamson55 <benjaminawilliamson@gmail.com>
2023-07-06 17:00:10 -0400

    Hooks Service Layer
    - Update hook controller delete class with new service layer

--- Hooks/Controller/Adminhtml/ManageHooks/Delete.php
+++ Hooks/Controller/Adminhtml/ManageHooks/Delete.php
@@ -3,32 +3,43 @@
 namespace Bwilliamson\Hooks\Controller\Adminhtml\ManageHooks;
 
 use Bwilliamson\Hooks\Controller\Adminhtml\AbstractManageHooks;
-use Bwilliamson\Hooks\Model\Hook;
 use Exception;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Backend\App\Action\Context;
+use Bwilliamson\Hooks\Api\HooksRepositoryInterface;
 
 class Delete extends AbstractManageHooks
 {
-    public function execute()
+    protected ?HooksRepositoryInterface $hooksRepository;
+
+    public function __construct(
+        Context $context,
+        ?HooksRepositoryInterface $hooksRepository = null
+    ) {
+        parent::__construct($context);
+        $this->hooksRepository = $hooksRepository;
+    }
+
+    public function execute(): ResultInterface|ResponseInterface|Redirect
     {
         $resultRedirect = $this->resultRedirectFactory->create();
         $hook = $this->initHook();
         if ($hook->getId()) {
             try {
-                /** @var Hook $hook */
-                $hook->delete();
-
-                $this->messageManager->addSuccess(__('The Hook has been deleted.'));
-                $resultRedirect->setPath('bwhooks/*/');
-
-                return $resultRedirect;
-            } catch (Exception $e) {
-                $this->messageManager->addError($e->getMessage());
-                $resultRedirect->setPath('bwhooks/*/edit', ['hook_id' => $hook->getId()]);
-
-                return $resultRedirect;
+                $this->hooksRepository->deleteById($hook->getId());
+                $this->messageManager->addSuccessMessage(__('The Hook has been deleted.'));
+            } catch (LocalizedException $e) {
+                $this->messageManager->addErrorMessage($e->getMessage());
+            } catch (Exception) {
+                $this->messageManager->addErrorMessage(__('An error occurred while deleting the Hook.'));
             }
+        } else {
+            $this->messageManager->addErrorMessage(__('The Hook to delete was not found.'));
         }
-        $this->messageManager->addError(__('The Hook to delete was not found.'));
+
         $resultRedirect->setPath('bwhooks/*/');
 
         return $resultRedirect;
